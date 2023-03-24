f4f81a8b75d9f2fea5bca7a117aed919d1436343
Gina <gprovenc@unca.edu>
2023-03-24 12:22:08 -0400

    tutorial

---
+++ tutorials/tutorial07/02-theme-switcher/index.js
@@ -0,0 +1,33 @@
+function defaultTheme() {
+    // your code here.
+    console.log('update to show default theme');
+    document.querySelector('body').className ='';
+}
+
+function oceanTheme() {
+   // your code here.
+   console.log('update to show ocean theme');
+    document.querySelector('body').className ='ocean';
+}
+
+function desertTheme() {
+   // your code here.
+   console.log('update to show desert theme');
+   document.querySelector('body').className ='desert';
+}
+
+function highContrastTheme() {
+    // your code here.
+    console.log('update to show high contrast theme');
+    document.querySelector('body').className ='high-contrast';
+}
+
+/*
+    Hints: 
+    1. Attach the event handlers (functions) above to the click event
+       of each of the four buttons (#default, #ocean, #desert, 
+        and #high-contrast) in index.html.
+    2. Then, modify the  body of each function so that it
+       sets the className property of the body tag based on 
+       the button that was clicked.
+*/
